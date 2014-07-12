ab564faf3929859853203ed61836e60125a4f923
Paul Conway <paulconway0@gmail.com>
2014-07-12 17:44:43 +0100

    Roll tweak, Lockon implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	public float speed, rollForce;
 	float vert, horiz;
 	Vector3 velocity = Vector3.zero;
 	Vector3 forceAcc = Vector3.zero;
@@ -14,12 +14,21 @@ public class PlayerController : MonoBehaviour {
 	Vector3 lookAt;
 	Vector3 rollDir;
 	bool isRolling, rechargeStam;
-	bool isFocusing;
+	bool lockedOn;
 
 	float currHealth = 100f, maxHealth = 100f;
 	float currStam = 100f, maxStam = 100f;
 
 	public Texture healthOver, healthUnder, stamOver, stamUnder;
+
+	private Transform _transform;
+	private CharacterController _controller;
+
+	private void Awake()
+	{
+		_controller = GetComponent<CharacterController>();
+		_transform = this.transform;
+	}
 
 	void Start () 
 	{
@@ -28,6 +37,8 @@ public class PlayerController : MonoBehaviour {
 
 	void Update () 
 	{
+
+		Debug.Log(rigidbody.velocity.magnitude);
 		vert = Input.GetAxisRaw ("Vertical");
 		horiz = Input.GetAxisRaw ("Horizontal");
 		//rollDir = transform.TransformDirection(new Vector3(transform.position.x, 0f, transform.position.z)).normalized; //direction of movement or facing direction?
@@ -47,44 +58,55 @@ public class PlayerController : MonoBehaviour {
 		{
 			if(Input.GetKey (KeyCode.D) || Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("HorizDPad") > 0)
 			{
-				transform.Translate(Vector3.right * speed * Time.deltaTime);
-				rollDir = Vector3.right;
-				transform.GetChild(0).transform.localScale = new Vector3(1.6f, 1.6f, 1.6f);
+				_controller.Move (Vector3.right * speed * Time.deltaTime);
+				if(!lockedOn)
+				{
+					rollDir = Vector3.right;
+					_transform.GetChild(0).transform.localScale = new Vector3(1.6f, 1.6f, 1.6f);
+				}
 			}
 			if(Input.GetKey (KeyCode.A) || Input.GetAxisRaw("Horizontal") < 0 || Input.GetAxisRaw("HorizDPad") < 0)
 			{
-				transform.Translate(-Vector3.right * speed * Time.deltaTime);
-				rollDir = -Vector3.right;
-				transform.GetChild(0).transform.transform.localScale = new Vector3(-1.6f, 1.6f, 1.6f);
+				_controller.Move (-Vector3.right * speed * Time.deltaTime);
+				if(!lockedOn)
+				{
+					rollDir = -Vector3.right;
+					_transform.GetChild(0).transform.localScale = new Vector3(-1.6f, 1.6f, 1.6f);
+				}	
 			}
 			if(Input.GetKey (KeyCode.W) || Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("VertDPad") > 0)
 			{
-				transform.Translate(Vector3.forward * speed * Time.deltaTime);
+				_controller.Move (Vector3.forward * speed * Time.deltaTime);
+				if(!lockedOn)
 				rollDir = Vector3.forward;
 			}
 			if(Input.GetKey (KeyCode.S) || Input.GetAxisRaw("Vertical") < 0 || Input.GetAxisRaw("VertDPad") < 0)
 			{
-				transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+				_controller.Move (-Vector3.forward * speed * Time.deltaTime);
+				if(!lockedOn)
 				rollDir = -Vector3.forward;
 			}
 
-			if((Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.W)) || (Input.GetAxisRaw("HorizDPad") < 0 && Input.GetAxisRaw("VertDPad") > 0))
+			if(!lockedOn)
 			{
-				rollDir = new Vector3(-1, 0, 1);
+				if((Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.W)) || (Input.GetAxisRaw("HorizDPad") < 0 && Input.GetAxisRaw("VertDPad") > 0))
+				{
+					rollDir = new Vector3(-1, 0, 1);
+				}
+				if((Input.GetKey (KeyCode.D) && Input.GetKey (KeyCode.W)) || (Input.GetAxisRaw("HorizDPad") > 0 && Input.GetAxisRaw("VertDPad") > 0))
+				{
+					rollDir = new Vector3(1, 0, 1);
+				}
+				if((Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.S)) || (Input.GetAxisRaw("HorizDPad") < 0 && Input.GetAxisRaw("VertDPad") < 0))
+				{
+					rollDir = new Vector3(-1, 0, -1);
+				}
+				if((Input.GetKey (KeyCode.D) && Input.GetKey (KeyCode.S)) || (Input.GetAxisRaw("HorizDPad") > 0 && Input.GetAxisRaw("VertDPad") < 0))
+				{
+					rollDir = new Vector3(1, 0, -1);
+				}
+				hitDetect.position = _transform.position + rollDir; //sets the location of the OverlapSphere for attacking direction
 			}
-			if((Input.GetKey (KeyCode.D) && Input.GetKey (KeyCode.W)) || (Input.GetAxisRaw("HorizDPad") > 0 && Input.GetAxisRaw("VertDPad") > 0))
-			{
-				rollDir = new Vector3(1, 0, 1);
-			}
-			if((Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.S)) || (Input.GetAxisRaw("HorizDPad") < 0 && Input.GetAxisRaw("VertDPad") < 0))
-			{
-				rollDir = new Vector3(-1, 0, -1);
-			}
-			if((Input.GetKey (KeyCode.D) && Input.GetKey (KeyCode.S)) || (Input.GetAxisRaw("HorizDPad") > 0 && Input.GetAxisRaw("VertDPad") < 0))
-			{
-				rollDir = new Vector3(1, 0, -1);
-			}
-			hitDetect.position = transform.position + rollDir; //sets the location of the OverlapSphere for attacking direction
 
 			if(Input.GetButtonDown ("Fire1"))
 			{
@@ -97,10 +119,19 @@ public class PlayerController : MonoBehaviour {
 					StartCoroutine("staminaChargeDelay");
 				}
 			}
+			if(Input.GetButtonDown ("LockOn"))
+			{
+				Debug.Log("Locked");
+				speed = 3f;
+				lockedOn = true;
+			}
+			if(Input.GetButtonUp ("LockOn"))
+			{
+				lockedOn = false;
+				speed = 6f;
+			}
 		}
-
-
-		if(rechargeStam)
+		if(rechargeStam) //stamina recharging
 		{
 			currStam += 25f * Time.deltaTime;
 			if(currStam >= maxStam)
@@ -108,6 +139,40 @@ public class PlayerController : MonoBehaviour {
 				currStam = maxStam;
 				rechargeStam = false;
 			}
+		}
+
+		RaycastHit hitRoll;
+		if(Physics.Raycast(_transform.position, rollDir, out hitRoll, 1f))
+		{
+			Debug.Log ("Not Rolling");
+			if(isRolling)
+			{
+				rigidbody.velocity = Vector3.zero;
+			}
+		}
+		else
+		{
+			if(!isRolling)
+			{
+				if(currStam >= 20)
+				{
+					if(Input.GetButtonDown("Roll"))
+					{
+						StopCoroutine("rollTimer");
+						isRolling = true;
+						rechargeStam = false;
+						StartCoroutine("rollTimer");
+						currStam -= 20f;
+					}
+				}
+			}
+		}
+
+		//_controller.Move (rollDir * speed * rollForce * Time.deltaTime); //moving the charController
+
+		if(_transform.position.y > -5.46f) //check if the player moves up on Y axis
+		{
+			_transform.position = new Vector3 (_transform.position.x, -5.46f, _transform.position.z);
 		}
 	}
 
@@ -119,19 +184,19 @@ public class PlayerController : MonoBehaviour {
 		{
 			Destroy(hitColliders[i].gameObject);
 		}
-		
 	}
 
 	void OnDrawGizmos() //for visual debug
 	{
 		Gizmos.color = Color.magenta;
-		//Gizmos.DrawSphere(playerSprite.transform.position+rollDir, .6f);
+		Gizmos.DrawSphere(playerSprite.transform.position+rollDir, .6f);
 	}
 	
 	IEnumerator rollTimer()
 	{
 		yield return new WaitForSeconds(.2f);
 		rigidbody.velocity = Vector3.zero;
+		transform.position = transform.position;
 		isRolling = false;
 		yield return new WaitForSeconds(.8f);
 		rechargeStam = true;
@@ -145,23 +210,12 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate ()
 	{
-		if(!isRolling)
+		if(isRolling)
 		{
-			if(currStam >= 20)
-			{
-				if(Input.GetButtonDown("Jump"))
-				{
-					StopCoroutine("rollTimer");
-					rechargeStam = false;
-					rigidbody.AddForce(rollDir * 16f , ForceMode.Impulse);
-					isRolling = true;
-					currStam -= 20f;
-					StartCoroutine("rollTimer");
-				}
-			}
+			rigidbody.AddForce(rollDir * rollForce, ForceMode.Impulse);
 		}
 	}
-
+	
 	/*void ForceIntegrator()
 	{
 		Vector3 accel = forceAcc / mass; //new vector for acceleration, which is the forceAcc divided by the mass of object
@@ -175,19 +229,19 @@ public class PlayerController : MonoBehaviour {
 
 		if(Input.GetAxisRaw ("Horizontal") > 0)
 		{
-			forceAcc += Vector3.right * speed; //adds force to make it move!		
+			forceAcc += Vector3.right * ; //adds force to make it move!		
 		}
 		if(Input.GetAxisRaw ("Horizontal") < 0)
 		{
-			forceAcc += -Vector3.right * speed; //adds force to make it move!		
+			forceAcc += -Vector3.right * ; //adds force to make it move!		
 		}
 		if(Input.GetAxisRaw ("Vertical") > 0)
 		{
-			forceAcc += Vector3.forward * speed; //adds force to make it move!		
+			forceAcc += Vector3.forward * ; //adds force to make it move!		
 		}
 		if(Input.GetAxisRaw ("Vertical") < 0)
 		{
-			forceAcc += -Vector3.forward * speed; //adds force to make it move!		
+			forceAcc += -Vector3.forward * ; //adds force to make it move!		
 		}
 		velocity *= 0.9f;
 		if(Input.GetAxisRaw ("Horizontal") == 0 && Input.GetAxisRaw ("Vertical") == 0)
